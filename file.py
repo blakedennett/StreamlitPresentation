66c2eb29678b6a8eac2fc252a4d2317b1d11ec79
blakedennet2 <den18019@byui.edu>
2023-12-05 06:22:47 -0700

    completed tutorial

--- file.py
+++ file.py
@@ -1,23 +1,21 @@
-# Streamlit Presentation
+import polars as pl
+import streamlit as st
 
+st.title("Website Title")
 
-### Headers
 
 st.header('This is the header')
 
-### Link
 
 st.markdown("[Source Code](https://github.com/blakedennett/LoanApprovalProject)")
-
-### Code Block
 
 code_block = """
 def func():
     print('hi')
 """
+
 st.code(code_block, language='python')
 
-### Table
 
 st.subheader("This is some nice data")
 
@@ -26,48 +24,21 @@ df = pl.read_csv(r'https://raw.githubusercontent.com/blakedennett/LoanApprovalPr
 st.table(df.limit(3))
 
 
-### Checkbox
-
 state = st.checkbox('Show Image', value=True)
 
-### Images
+
 
 if state:
     st.image('images/mountains.PNG', width=700)
 
 
-### CSS
-
-st.markdown("""
-<style>
-.element-container.st-emotion-cache-e8g64f.e1f1d6gn3 
-{
-    color: red;
-}
-</style>""", unsafe_allow_html=True)
-
-### Dropdown
-
 choice = st.radio("Select the place", options=['Las Vegas', 'Orlando'])
 st.write(choice)
 
-### Button
-
-def disp():
-    st.write('Hello there')
-
-but = st.button("Display Greeting", on_click=disp)
-
-### Dropdown
 
 select = st.selectbox("Select dog breed", options=('None Selected', 'Rotweiler', 'Border Collie'))
 
-### Text input
 
 put = st.text_input('Enter the income')
 
 st.write(put)
-
-### Line to run code in terminal
-
-streamlit run file.py
